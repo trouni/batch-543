586e7db7c5f0539ee76b9629615f7816bf331b02
Trouni Tiet <trouni@gmail.com>
2021-01-20 11:19:53 +0900

    Classes & Instances lecture

---
+++ lectures/oop/accessing_data.rb
@@ -0,0 +1,14 @@
+# Reminder about how to access data
+
+# Arrays
+car = ["red", 2021]
+cars[0] # read using index (square brackets)
+
+# Hashes
+car = { color: "red", year: 2021 }
+car[:color] # read using key (square brackets)
+
+# Instances of custom classes
+car = Car.new("red", 2021)
+car.color # read using instance method (dot notation)
+
