0a8a2605f1147be588b9970752db8897c3885354
Trouni Tiet <trouni@gmail.com>
2021-02-24 09:57:06 +0900

    Implement Mapbox and markers

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -3,6 +3,12 @@ class FlatsController < ApplicationController
 
   def index
     @flats = Flat.all
+    @markers = @flats.geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
+    end
   end
 
   def show
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,9 +26,9 @@ require("channels")
 import "bootstrap";
 
 // Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+import { initMapbox } from '../plugins/init_mapbox';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  // initSelect2();
+  initMapbox();
 });
